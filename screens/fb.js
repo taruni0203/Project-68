bf33fd04e9e5cfc96a28f9469e61b1299ec76030
taruni0203 <67770771+taruni0203@users.noreply.github.com>
2020-12-31 22:58:26 -0800

    Add files via upload

---
+++ screens/fb.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default class FacebookScreen extends React.Component {
+    render() {
+      return (
+        <View style={{ 
+            flex: 1, 
+            justifyContent: 'center', 
+            alignItems: 'center',
+            backgroundColor: 'lightblue' 
+            }}>
+          <Text style={{
+              color: '#ff5252',
+              fontWeight: 'bold',
+              fontFamily: 'cursive',
+              fontSize: 50
+          }}>
+              Facebook</Text>
+        </View>
+      );
+    }
+  }
